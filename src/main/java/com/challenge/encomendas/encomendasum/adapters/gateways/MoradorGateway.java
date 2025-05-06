16b60e40d955b4a4a91794e72d6053df2462d20c
Jnpacheco-125 <rm350127@fiap.com.br>
2025-05-06 11:16:21 -0300

    Buscar todos os moradores paginados

--- src/main/java/com/challenge/encomendas/encomendasum/adapters/gateways/MoradorGateway.java
+++ src/main/java/com/challenge/encomendas/encomendasum/adapters/gateways/MoradorGateway.java
@@ -1,6 +1,8 @@
 package com.challenge.encomendas.encomendasum.adapters.gateways;
 
 import com.challenge.encomendas.encomendasum.domain.entities.Morador;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +13,7 @@ public interface MoradorGateway {
     Optional<Morador> findByEmail(String email);
     Optional<Morador> findByTelefone(String telefone);
     Optional<Morador> findByApartamento(String apartamento);
-    List<Morador> findAll(); // Se precisar listar todos os moradores
+    Page<Morador> findAll(Pageable pageable);// Se precisar listar todos os moradores
     void deleteById(Long id);
 }
 
